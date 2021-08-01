24fab9effa15c71daa5dad1ba8063a59df6dbcc8
Alieyeh <Alieyeh@gmail.com>
2021-08-01 19:26:49 +0430

    added Dormitory folder

---
+++ Dormitory/dbo.City.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE TABLE [dbo].[City] (
+    [CityCode]    INT            NOT NULL,
+    [CityName]    NVARCHAR (100) NOT NULL,
+    [Province] NVARCHAR(100)            NOT NULL,
+    PRIMARY KEY CLUSTERED ([CityCode] ASC),
+    UNIQUE NONCLUSTERED ([CityName] ASC)
+);
+
+
---
+++ Dormitory/dbo.Users.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Mellicode] INT            NOT NULL,
+    [Email]  NVARCHAR (100) NOT NULL,
+    [Password]  NVARCHAR (100) NOT NULL,
+    [BirthDate] DATE           NOT NULL,
+    [Gender]    NVARCHAR (20)  NOT NULL,
+    [UAddress]   NVARCHAR (300) NOT NULL,
+    [FullName]      NVARCHAR (200) NOT NULL,
+    
+    PRIMARY KEY CLUSTERED ([Mellicode] ASC),
+    CHECK ([BirthDate]>='1900-01-01' AND [BirthDate]<='2020-01-01'),
+    CHECK ([Gender]='F' OR [Gender]='M')
+);
